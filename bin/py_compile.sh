b18ec16c4492e84ba32d4d7463db9905940d217c
Alexandre Boisvert <alexandre.boisvert.1@gmail.com>
2013-04-04 08:33:06 -0400

    Adding pyflakes to py_compile.sh.

--- bin/py_compile.sh
+++ bin/py_compile.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Python compilation
-# Check the syntax of the given file for PEP8 compliance and create 
-# a pyc file.
+# Check the syntax of the given file for PEP8 compliance and pyflakes
+# errors then create a pyc file.
 
 # Dependencies: pep8 (pip install pep8)
+# Dependencies: pyflakes (apt-get install pyflakes)
 
 # Minimal parameters validation
 if [ "$1" == "" ] ;
@@ -15,6 +16,9 @@ fi
 
 echo "PEP 8 Validation"
 pep8 "$1"
+
+echo "pyflakes Validation"
+pyflakes "$1"
 
 echo "Python Compilation"
 python -m py_compile "$1"
