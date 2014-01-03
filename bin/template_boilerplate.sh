36e90bdeb4705dde3f20d3afdac0c99fa7cd7393
Alexandre Boisvert <alexandre.boisvert.1@gmail.com>
2014-01-03 11:13:49 -0500

    Adding while loop to manage the arguments. Adding option to use another log file.

--- bin/template_boilerplate.sh
+++ bin/template_boilerplate.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 ######################################################################
-# Constants and global variables
+# Constants and global variables (default values)
 
 # Where to store the logs
-LOG_FILE="/tmp/template_boilerplate.txt"
+log_file="/tmp/template_boilerplate.txt"
 
 # Change this to 0 to only write to the log file
 verbose_mode=0
@@ -16,43 +16,68 @@ write_msg(){
     # $1: message to write
     date_log=$(date "+%Y-%m-%d %H:%M:%S")
     [ "$verbose_mode" -ne "0" ] && echo "$date_log $1"
-    echo "$date_log $1" >> $LOG_FILE
+    echo "$date_log $1" >> $log_file
 }
 
 stop_on_error(){
     # $1: $? the return code from the preceding statement
-    [ "$1" -ne "0" ] && write_msg "Error occured, exiting" && exit "$1"
+    [ "$1" -ne "0" ] && write_msg "Error occured ($1), exiting" && exit "$1"
 }
 
 ######################################################################
 # Argument parsing
 
 usage(){
-# Displays the short help message
-# EOF placement is important
-cat << EOF
+    # Displays the short help message
+    # EOF placement is important
+    cat << EOF
 
-Usage: $0 [-h] [-v]
+Usage: $0 [-h] [-v] [-l log_file]
 
 Options:
 - h: display this help message and exit.
 - v: the script becomes more verbose.
+- l: specify the log file name (default = $log_file)
 
 Typical use:
-$0 -v
+$0 -v -l ~/log.txt
 
 EOF
 }
 
-# TODO while loop over the arguments
-
 ######################################################################
 # Main program
 
-# Initialising the empty log file
-echo "" > $LOG_FILE
+# while loop over the arguments
+# x: ==> argument x requires a value
+while getopts "hl:v" OPTION
+do
+    case $OPTION in
+        h)
+            usage
+            exit 0
+            ;;
+        l)
+            log_file="$OPTARG"
+            ;;
+        v)
+            verbose_mode=1
+            ;;
+        # Handling the incorrect arguments
+        ?)
+            usage
+            exit 1
+            ;;
+        :)
+            usage
+            exit 1
+            ;;
+    esac
+done
 
-write_msg "first line"
+[ "$verbose_mode" -eq "1" ] && write_msg "Verbose mode activated"
+
+write_msg "log file location = $log_file"
 
 write_msg "valid operation"
 true
@@ -62,7 +87,7 @@ stop_on_error "$?"
 #false
 #stop_on_error "$?"
 
-usage >> $LOG_FILE
+#usage >> $log_file
 
 write_msg "End process"
 
