3325537c67bc6cfb61c4db5bb303dd953fee890f
Alex <ihatespam@spam.spam>
2014-11-06 19:17:23 -0500

    Linux-Dashboard: adding the external IP

--- bin/linux-dashboard.sh
+++ bin/linux-dashboard.sh
@@ -17,6 +17,11 @@ df -i
 header "IP addresses"
 ip a | grep inet | grep -v inet6
 
+header "External IP"
+echo "$(wget \
+--user-agent="Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 ( .NET CLR 3.5.30729)" \
+--quiet -O- http://ipecho.net/plain)"
+
 header "Uptime, memory, load, processes"
 top -n1 -b | head -n5
 
