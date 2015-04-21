7f08077b8ac863231bf1f205cb6923264d27caee
Alexandre Boisvert <alex@SL410>
2015-04-21 13:54:02 -0400

    Py Template: new code for syslog and some refactoring.

--- bin/template_boilerplate.py
+++ bin/template_boilerplate.py
@@ -1,25 +1,48 @@
 #!/usr/bin/env python
 
+# TODO read a config file
+# https://docs.python.org/2/library/configparser.html
+# or use JSON...
+
 # importing argparse since optparse is now deprecated
 # https://docs.python.org/2/library/argparse.html
 import argparse
 
-argument_parser = argparse.ArgumentParser(
-                      description="Template Boilerplate script",
-                      epilog="Typical use: ...")
-argument_parser.add_argument("-i", "--integer-value", default=0,
-                             type=int, required=False,
-                             help="an integer of your choice",
-                             metavar="I", dest="int_value")
-argument_parser.add_argument("-c", "--config-file", default=None,
-                             type=str, required=False,
-                             help="The path to the config file to use",
-                             metavar="C", dest="config_file_name")
+# syslog writing utilities
+import syslog
+
+# syslog program name (instead of sys.argv[0])
+PROGRAM_NAME = "template_boilerplate_py"
 
 
-arguments_container = argument_parser.parse_args()
+# syslog functions
+def log_info(message):
+    """Write an informational event to syslog"""
+    syslog.syslog(message)
+
+
+def log_err(message):
+    """Write an error event to syslog"""
+    syslog.syslog(syslog.LOG_ERR, message)
 
 if __name__ == "__main__":
+    argument_parser = argparse.ArgumentParser(description="Template Boilerplate script",
+        epilog="Typical use: ...")
+    argument_parser.add_argument("-i", "--integer-value", default=0,
+        type=int, required=False,
+        help="an integer of your choice",
+        metavar="I", dest="int_value")
+    argument_parser.add_argument("-c", "--config-file", default=None,
+        type=str, required=False,
+        help="The path to the config file to use",
+        metavar="C", dest="config_file_name")
+
+    arguments_container = argument_parser.parse_args()
+
+    # syslog, set the program name before starting
+    syslog.openlog(ident=PROGRAM_NAME)
+    log_info("Starting the program")
+
     print "Template boilerplate Python script"
     print "int value =", arguments_container.int_value
     if arguments_container.config_file_name is None:
@@ -27,11 +50,5 @@ if __name__ == "__main__":
     else:
         print "Config file provided =", arguments_container.config_file_name
 
-# TODO read a config file
-# https://docs.python.org/2/library/configparser.html
-
-# TODO write a log file
-# https://docs.python.org/2/howto/logging.html
-
-# TODO write to syslog
-# https://docs.python.org/2/library/syslog.html
+    log_err("Simulating an error")
+    log_info("Ending the program")
