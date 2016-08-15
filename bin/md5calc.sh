c1c16ddc7425dc8b168eece6b9d0f3957696f640
alex <alex.beast@example.com>
2016-08-15 19:31:29 -0400

    MD5CALC: now a script instead of an alias

---
+++ bin/md5calc.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+######################################################################
+# Constants and global variables (default values)
+
+# recursively look for files in this directory
+dir_to_process=""
+
+######################################################################
+# Utility functions
+
+write_msg(){
+    # $1: message to write
+    date_log=$(date "+%Y-%m-%d %H:%M:%S")
+    echo "$date_log $1"
+}
+
+# We need to export this function to use in "find".
+export -f write_msg
+
+process_md5calc(){
+    # $1 = MD5 file name
+    write_msg "processing $1"
+    md5sum --quiet -c "$1"
+}
+
+# We need to export this function to use in "find".
+export -f process_md5calc
+
+######################################################################
+# Argument parsing
+
+usage(){
+    # Displays the short help message
+    # EOF placement is important
+    cat << EOF
+
+Compute all the MD5 checksums in the specified directory
+
+Usage: $0 [-h] [-d dir_name]
+
+Options:
+- h: display this help message and exit.
+- d: the directory to search (defaults to current directory)
+
+Typical use:
+$0 -d ~/data_files/
+
+cd ~/my_files/
+$0
+
+EOF
+}
+
+######################################################################
+# Main program
+
+# while loop over the arguments
+# x: ==> argument x requires a value
+while getopts "hd:" OPTION
+do
+    case $OPTION in
+        h)
+            usage
+            exit 0
+            ;;
+        d)
+            dir_to_process="$OPTARG"
+            ;;
+        # Handling the incorrect arguments
+        ?)
+            usage
+            exit 1
+            ;;
+        :)
+            usage
+            exit 1
+            ;;
+    esac
+done
+
+if [ -z "${dir_to_process}" ]
+then
+    write_msg "No directory specified, defaulting to '.'"
+    dir_to_process='.'
+fi
+
+write_msg "Processing dir '${dir_to_process}'"
+
+find "${dir_to_process}" -type f -iname '*.md5' -execdir bash -c 'process_md5calc "$1"' _ {} \;
+
+write_msg "End process, see above if any error message"
+
+exit 0
