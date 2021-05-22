ebfdb69eb4c4786a9770b556da32dd6813f71d76
alex.hptower@example.com <alex.hptower@example.com>
2021-05-21 20:45:00 -0400

    Better handling of incorrect parameters and stopping on first error.

--- bin/code_validation/perl_compile.sh
+++ bin/code_validation/perl_compile.sh
@@ -11,6 +11,15 @@ then
     exit 1
 fi
 
+if ! [ -r "$1" ]
+then
+    echo "Usage: $0 file_to_check.pl" >  /dev/stderr
+    exit 1
+fi
+
+# stop on any error
+set -e
+
 echo "Perl checks"
 perl -w -W -t -T -c "$1"
 
