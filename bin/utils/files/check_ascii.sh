8c9179fee1aa9c106d5e723deed800413b77a0e2
alex.hptower@example.com <alex.hptower@example.com>
2021-05-23 16:14:20 -0400

    adding color to the output

--- bin/utils/files/check_ascii.sh
+++ bin/utils/files/check_ascii.sh
@@ -4,4 +4,4 @@
 # Useful for source code files.
 # All characters outside the range [0-127] will be spotted
 
-cat --number "$1" | grep --perl-regexp '[^\x00-\x7F]'
+cat --number "$1" | grep --color=always --perl-regexp '[^\x00-\x7F]'
