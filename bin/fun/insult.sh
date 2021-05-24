f833b986d3640d9fd92493806b1720241dd81709
alex.hptower@example.com <alex.hptower@example.com>
2021-05-24 19:49:03 -0400

    adding a new script: insult.sh

---
+++ bin/fun/insult.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# inspired by https://www.reddit.com/r/AskReddit/comments/k8f2sa/what_is_your_best_insult_without_swearing/.compact
+
+insults="I'd call you dumb as a rock, but at least a rock can hold a door open.
+I'm lonely not desperate.
+I bet your parents change the subject when their friends ask about you.
+Where have you been all my life? I don't know, but I wish I was still there.
+I envy the people who havent met you.
+My days of not taking you seriously are certainly coming to a middle.
+You're a conversation starter. Not when you are around but once you leave.
+Some cause happiness wherever they go; others whenever they go. (Oscar Wilde).
+If you were the light at the end of the tunnel, I'd turn back around.
+I wish for just one time you could stand in my shoes-you'd know what a drag it is to see you. Bob Dylan - Positively Fourth Street.
+I'd give you a nasty look but you've already got one.
+I'm genuinely excited to never interact with you again.
+I find the fact that you lived this long both surprising and disappointing.
+If I throw a stick, will you leave?
+You're impossible to underestimate.
+You are the reason why shampoo has instructions.
+You have a great face for radio.
+Somewhere somehow you are robbing a village of an idiot.
+Everyone who ever loved you was wrong.
+Your face makes blind kids cry.
+You would have been better as a stain between the bedsheets."
+
+echo "${insults}" | shuf -n1
+
