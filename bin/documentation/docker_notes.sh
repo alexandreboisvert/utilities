6e747d1de798901b69bf6fb580409dd2c3ce7026
alex <alex-raspi@example.com>
2021-06-08 10:57:20 -0400

    docker doc: better names for the directories

--- bin/documentation/docker_notes.sh
+++ bin/documentation/docker_notes.sh
@@ -17,7 +17,7 @@ docker run -i -t 000_image_name_000 /bin/bash
 # start with a shared directory
 # /tmp: directory on the host
 # /tmp/HOST: directory inside docker
-docker run -i -t -v /tmp:/tmp/HOST 000_image_name_000 /bin/bash
+docker run -i -t -v /tmp/TO_DOCKER:/tmp/TO_HOST 000_image_name_000 /bin/bash
 
 # start an stopped container and attach a terminal
 docker start -i -t 999_container_name_999 /bin/bash
