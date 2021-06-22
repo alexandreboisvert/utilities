120fc612c402cc2da09376558080b026cea01c03
alex.hptower@example.com <alex.hptower@example.com>
2021-06-22 17:12:23 -0400

    improv: list of utilities in docker

--- bin/documentation/docker_notes.sh
+++ bin/documentation/docker_notes.sh
@@ -25,4 +25,7 @@ docker start -i -t 999_container_name_999 /bin/bash
 # attach a terminal to a running container
 docker exec -i -t 999_container_name_999 /bin/bash
 
+# inside an Ubuntu/Debian docker, try installing some utilities
+apt install bash-completion bzip2 cifs-utils ethtool exfat-fuse exfat-utils gawk gdebi-core git gzip html2text htop iotop iperf locate lsof mc oping p7zip p7zip-full p7zip-rar parted pv rsync shellcheck tar tcpdump telnet tmux tree unzip util-linux vim
+
 EOF
