a1608786896ec7741edded2d32d8f02010bc2f60
alex.hptower@example.com <alex.hptower@example.com>
2021-07-31 07:33:08 -0400

    add: vim documentation

---
+++ bin/documentation/vim_notes.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+cat << EOF
+
+# AUTOCOMPLETE
+CTRL + n
+
+# SPELLCHECK
+# enable spellcheck (vim-spell-en package required)
+:set spell spelllang=en_us
+# move to the misspelled word and type
+z=
+
+EOF
