d3a86eb55ec33fb836ba92dcdbccb290218ed4a6
alex.hptower@example.com <alex.hptower@example.com>
2021-08-17 19:54:08 -0400

    fix: full path for go executable

--- bin/code_validation/go_check_version.sh
+++ bin/code_validation/go_check_version.sh
@@ -3,7 +3,9 @@
 # Script to compare the locally installed version to the current
 # version on the website.
 
-local_go_version=$(go version | grep --only-matching --extended-regexp '[0-9]+\.[0-9]+(\.[0-9]+)*')
+# using go full path for the executable
+# makes the script more portable
+local_go_version=$(/usr/local/go/bin/go version | grep --only-matching --extended-regexp '[0-9]+\.[0-9]+(\.[0-9]+)*')
 
 current_go_version=$(curl --silent --user-agent "mozilla" 'https://golang.org/dl/' \
   | grep --extended-regexp '<span class="filename">.*src.*' \
