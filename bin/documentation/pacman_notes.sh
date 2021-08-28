b95a872c063f72379fb5ca0297d7a3540c547900
alex.hptower@example.com <alex.hptower@example.com>
2021-08-28 13:52:54 -0400

    add: pacman and apt documentation

---
+++ bin/documentation/pacman_notes.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+cat <<EOF
+
+# From https://wiki.archlinux.org/title/Pacman/Rosetta
+
+# Install a package(s) by name
+pacman -S
+
+# Remove a package(s) by name
+pacman -Rs
+
+# Search for package(s) by name/description/short description
+pacman -Ss
+
+# Upgrade Packages - Install packages which have an older version already installed
+pacman -Syu
+
+# Show all or most information about a package.
+pacman -Si # remote query
+pacman -Qi # local query
+
+EOF
