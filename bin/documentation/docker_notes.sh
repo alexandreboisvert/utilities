80d9b27d27aebf90a7e4b2df994c66cf225e9f0e
alex.hptower@example.com <alex.hptower@example.com>
2021-09-21 21:48:54 -0400

    add: memory option in docker

--- bin/documentation/docker_notes.sh
+++ bin/documentation/docker_notes.sh
@@ -28,4 +28,6 @@ docker exec -i -t 999_container_name_999 /bin/bash
 # inside an Ubuntu/Debian docker, try installing some utilities
 apt install bash-completion bzip2 cifs-utils curl ethtool exfat-fuse exfat-utils gawk gdebi-core git gzip html2text htop iotop iperf locate lsof mc oping p7zip p7zip-full p7zip-rar parted pv rsync shellcheck tar tcpdump telnet tmux tree unzip util-linux vim wget
 
+# if needed --memory 500m
+
 EOF
