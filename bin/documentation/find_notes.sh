5ed33ef3f9dd727149e6573a79beb87f7e129fec
alex.hptower@example.com <alex.hptower@example.com>
2021-09-30 20:01:53 -0400

    add: find notes, finding newer files

--- bin/documentation/find_notes.sh
+++ bin/documentation/find_notes.sh
@@ -12,6 +12,9 @@ find /tmp -type f -iname '*report*.txt'
 # find latest file in all sub directories
 find . -type f -iname '*.deb'  -printf '%C@ %p\n' | sort -n
 
+# find files newer than a given date
+find . -type f -iname '*.deb' -newermt '1999-12-31'
+
 # git pull on all git directories
 # execdir: excute the command in the directory (cd into directory)
 # execdir: the \; is important
