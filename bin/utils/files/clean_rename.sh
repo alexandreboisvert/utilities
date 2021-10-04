a2cbb4e572224e54d5d857d2d683a41906fbb972
alex.hptower@example.com <alex.hptower@example.com>
2021-10-04 19:40:31 -0400

    new: rename files with clean name

---
+++ bin/utils/files/clean_rename.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Rename each provided file with a more "acceptable" name
+
+if [ -z "$1" ]
+then
+  echo "provide a list of files"
+  echo 'Replace all the characters that are not "[:alnum:].-/" with "_"'
+  echo 'If the name is OK do not attempt the mv command'
+  exit 1
+fi
+
+for current_name in "$@"
+do
+  # Important to have . and / in the regex, without it the results
+  # could be incorrect.
+  new_name=$(echo -n "$current_name" |  tr -c '[:alnum:].-/' '_')
+  if [ "$new_name" == "$current_name" ]
+  then
+    echo "clean name already: $current_name"
+  else
+    mv -v "$current_name" "$new_name"
+  fi
+done
