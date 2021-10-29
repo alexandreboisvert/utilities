16436fb9cb47d0a4b97b517ab95e33f2fc698d05
alex <alex-raspi@example.com>
2021-10-29 13:03:44 -0400

    fix: new formatting, indicating golang version

--- bin/code_validation/go_check_version.sh
+++ bin/code_validation/go_check_version.sh
@@ -13,8 +13,8 @@ current_go_version=$(curl --max-time 5 --silent --user-agent "mozilla" 'https://
 
 if [[ "${local_go_version}" = "${current_go_version}" ]]
 then
-    echo "[  OK  ] local ($local_go_version) == current ($current_go_version)"
+    echo "[  OK  ] golang version: local ($local_go_version) == current ($current_go_version)"
 else
-    echo "[ FAIL ] NOT UP TO DATE, local ($local_go_version) != current ($current_go_version)"
+    echo "[ FAIL ] golang version NOT UP TO DATE: local ($local_go_version) != current ($current_go_version)"
     exit 1
 fi
