da86cf7ed144f379348aefd22bf24afe45cfbffd
alex.hptower@example.com <alex.hptower@example.com>
2021-11-11 18:19:03 -0500

    add: ffmpeg documentation

--- bin/documentation/ffmpeg_notes.sh
+++ bin/documentation/ffmpeg_notes.sh
@@ -23,4 +23,42 @@ fmpeg -i audio_and_video.mp4 -c copy -an video_only.mp4
 # NEVER TESTED, memory allocation problems
 ffmpeg -i input.mp4 -vf reverse -af areverse reversed_video.mp4
 
+# audio normalization
+
+bytesandbones.wordpress.com
+****** Audio normalization with ffmpeg using loudnorm (ebur128) filter ******
+Source: https://developers.google.com/actions/tools/audio-loudness
+Source: http://k.ylo.ph/2016/04/04/loudnorm.html (Credits: k@ylo.ph | Source
+discovered by Mulvya)
+    * ./ffmpeg -i /path/to/input.wav -af loudnorm=I=-16:TP=-1.5:LRA=11:
+      print_format=summary -f null -
+      This instructs FFmpeg to measure the audio values of your media file
+      without creating an output file. You will get a series of values
+      presented as follows:
+      Input Integrated:    -27.2 LUFS
+      Input True Peak:     -14.4 dBTP
+      Input LRA:             0.1 LU
+      Input Threshold:     -37.7 LUFS
+
+      Output Integrated:   -15.5 LUFS
+      Output True Peak:     -2.7 dBTP
+      Output LRA:            0.0 LU
+      Output Threshold:    -26.2 LUFS
+
+      Normalization Type:   Dynamic
+      Target Offset:        -0.5 LU
+      The sample values above indicate important information about the incoming
+      media. For instance, the Input Integrated value shown indicates audio
+      that is too loud. The Output Integrated value is much closer to -16.0.
+      Both the Input True Peak and Input LRA, or loudness range, values are
+      higher than our provided ceilings and will be reduced in the normalized
+      version. Finally, the Target Offset represents the offset gain used in
+      the output.
+    * Run a second pass of the loudnorm filter, supplying the values from step
+      1 as "measured" values in the loudnorm options.
+      ./ffmpeg -i /path/to/input.wav -af loudnorm=I=-16:TP=-1.5:LRA=11:
+      measured_I=-27.2:measured_TP=-14.4:measured_LRA=0.1:measured_thresh=-
+      37.7:offset=-0.7:linear=true:print_format=summary output.wav
+
+
 EOF
