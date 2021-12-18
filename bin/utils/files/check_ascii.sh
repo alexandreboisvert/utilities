10ee89fab1a3a8b524dd4318f79ea30f549d562f
alex.hptower@example.com <alex.hptower@example.com>
2021-12-17 22:05:39 -0500

    enh: can now process STDIN or a file

--- bin/utils/files/check_ascii.sh
+++ bin/utils/files/check_ascii.sh
@@ -1,7 +1,47 @@
 #!/usr/bin/env bash
 
-# Check for Non-ASCII characters in file.
-# Useful for source code files.
-# All characters outside the range [0-127] will be spotted
+usage() {
+  cat << EOF
 
-grep --line-number --color=always --perl-regexp '[^\x00-\x7F]' "$1"
+Check for Non-ASCII characters in file or STDIN
+
+Useful for source code files.
+
+All characters outside the range [0-127] will be spotted
+
+Options
+
+help                    : display this help and exit
+stdin                   : process the content of STDIN (to be used in pipes)
+files a.txt b.txt c.txt : process all the files a.txt, b.txt and c.txt.
+
+EOF
+}
+
+if [ -z "$1" ]
+then
+ usage
+ exit 1
+fi
+
+case "$1" in
+  help)
+    usage
+    exit 0
+    ;;
+  stdin)
+    grep --line-number --color=always --perl-regexp '[^\x00-\x7F]'
+    ;;
+  files)
+    shift
+    for filename in "$@"
+    do
+      grep --with-filename --line-number --color=always --perl-regexp '[^\x00-\x7F]' "$filename"
+    done
+    ;;
+  # Handling the incorrect arguments
+  *)
+    usage
+    exit 1
+    ;;
+esac
