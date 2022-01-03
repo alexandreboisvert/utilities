a48258dc10478f1b3279331e53db141bf7043987
alex.hptower@example.com <alex.hptower@example.com>
2022-01-03 18:30:49 -0500

    fix: better output format for irregular filenames

--- bin/utils/video/mediainfo_short.sh
+++ bin/utils/video/mediainfo_short.sh
@@ -3,30 +3,31 @@
 if [ -z "$1" ]
 then
   echo "Get a short summary from mediainfo"
+  echo "Pass as arguments the file names"
+  echo "Output:"
   echo "Width (in pixels) [tab] Height (in pixels) [tab] Duration (in milliseconds) [tab] Duration (in H and M) [tab] File Size (in bytes) [tab] File Name"
   echo "Requires the program mediainfo"
   exit 0
 fi
 
-for item in "$@"
+for filename in "$@"
 do
-  mediainfo --full "${item}" \
+  output=$(mediainfo --full "${filename}" \
   | awk 'BEGIN{
     width_pixel = 0;
     height_pixel = 0;
     duration_ms = 0;
     size_bytes = 0;
-    file_name = "no name";
   }
   /^Width +: [0-9]+$/ { width_pixel = $NF }
   /^Height +: [0-9]+$/ { height_pixel = $NF }
   /^Duration +: [0-9]+$/ { duration_ms = $NF }
   /^File size +: [0-9]+$/ { size_bytes = $NF }
-  /^Complete name +: .*$/ { file_name = $NF }
   END{
     duration_s = duration_ms / 1000.0;
     duration_h = int(duration_s / 3600.0);
     duration_m = int(((duration_s / 3600.0) - duration_h) * 60.0);
-    print width_pixel "\t" height_pixel "\t" duration_ms "\t" duration_h "h" duration_m "m\t" size_bytes "\t" file_name;
-  }'
-done
+    print width_pixel "\t" height_pixel "\t" duration_ms "\t" duration_h "h" duration_m "m\t" size_bytes;
+  }')
+  printf "%s\t%s\n" "$output" "$filename"
+done | column --table --table-columns-limit 6
