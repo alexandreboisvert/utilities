89b7aea7c21a9fcf63dfad2b2292786e7d9804bf
alex.hptower@example.com <alex.hptower@example.com>
2022-01-15 15:00:05 -0500

    add: new script for w3m with proper options

---
+++ bin/utils/network/web.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+w3m -no-mouse -no-cookie -no-graph -s -W "$@"
+
+#     -no-mouse        don't use mouse
+#     -cookie          use cookie (-no-cookie: don't use cookie)
+#     -no-graph        use ASCII character for border of table and menu
+#     -s               squeeze multiple blank lines
+#     -W               toggle search wrap mode
