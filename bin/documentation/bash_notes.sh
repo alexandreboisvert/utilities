81a0cec38109ebab51e60b81011e55aa49aaed3f
alex.hptower@example.com <alex.hptower@example.com>
2022-02-01 19:02:34 -0500

    add: bash notes on regex

--- bin/documentation/bash_notes.sh
+++ bin/documentation/bash_notes.sh
@@ -45,6 +45,7 @@ done
 ######################################################################
 # using [[ ]] instead of [ ]
 # see http://mywiki.wooledge.org/BashGuide/SpecialCharacters
+# see http://mywiki.wooledge.org/BashFAQ/031
 
 ######################################################################
 # string splitting
@@ -55,5 +56,23 @@ echo "\${input_string%% *}"
 echo "\${input_string##* }"
 # greedy %% and ##
 # non greedy % and #
+
+######################################################################
+# regex in bash
+
+echo "blah 1234 blah" > a.txt
+echo "bluh 1234 bluh" >> a.txt
+echo "blah blah blah" >> a.txt
+
+while read -r current_line
+do
+  # pro tip: use perl, this is a poor choice
+  # must use [[:space:]] instead of a space
+  if [[ "\$current_line" =~ ^blah[[:space:]](.*)[[:space:]]blah$ ]]
+  then
+    echo "found: \${BASH_REMATCH[0]}"
+    echo "only the middle: \${BASH_REMATCH[1]}"
+  fi
+done < a.txt
 
 EOF
