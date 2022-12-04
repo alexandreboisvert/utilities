89874f428f2a4c7799404838245d087928c496ec
alex.prodesk@example.com <alex.prodesk@example.com>
2022-12-03 21:50:15 -0500

    add and order famous last words

--- bin/fun/famous_last_words.sh
+++ bin/fun/famous_last_words.sh
@@ -2,30 +2,42 @@
 
 # Inspired by: https://www.reddit.com/r/AskReddit/comments/n917rz/apart_from_life_is_short_what_other_lines_do_you/.compact
 
-last_words="Life is short.
-Not all decisions i make will be right and this one will be no exception.
-What's the worst that could happen?
-That's for future me to worry about.
-Why not?
+last_words="C'est la vie.
+Can't make it any worse.
 FUCK IT WE'RE DOING IT LIVE!
-It won't be that bad, right?
-I wanna sound cool for my grandkids.
-In for a penny, in for a pound.
-Might as well see where this goes.
-What could possibly go wrong?
-If anything goes sideways, [insert friends name here] can help me.
-Hold my beer.
-I'll be fine.
-Leeeerooyyy...JENNNNNNKINNNNNNNSSS!
-I don't care anymore.
-Whatever.
-Is this a bad decision? Probably. Will I go ahead with it? Absolutely!
-C'est la vie.
-I'm just gonna wing it!
+Fortune favors the bold.
 God hates a coward.
-I'm here for a good time, not a long time.
+Here goes nothing.
+Hold my beer.
+I don't care anymore.
+I wanna sound cool for my grandkids.
 I will go with my gut.
+I'll be fine.
 I'll figure it out later.
-Not like I'm gonna make it out of life alive anyways"
+I'm here for a good time, not a long time.
+I'm just gonna wing it!
+If anything goes sideways, [insert friends name here] can help me.
+If we don't sin then Jesus died for nothing.
+In for a penny, in for a pound.
+Is this a bad decision? Probably. Will I go ahead with it? Absolutely!
+It is what it is.
+It won't be that bad, right?
+It would have happened sooner or later.
+It'll be good for the economy.
+Leeeerooyyy...JENNNNNNKINNNNNNNSSS!
+Let's see what happens.
+Life is short.
+Might as well see where this goes.
+Not all decisions i make will be right and this one will be no exception.
+Not like I'm gonna make it out of life alive anyways
+That's for future me to worry about.
+The only difference between confidence and stupidity is whether or not it works.
+What could possibly go wrong?
+What's a fart when you've already shat yourself?
+What's the worst that could happen?
+Whatever.
+Who wants to live forever?
+Why not?
+You only die once."
 
 echo "${last_words}" | shuf -n1
