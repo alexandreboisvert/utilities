6bc0a4e39307fc178c6b6d3e7d7c574131e2c400
alex <alex-raspi@example.com>
2023-05-26 20:22:11 -0400

    add: Perl skeleton file. Useful with vim autocmd BufNewFile

---
+++ bin/code_templates/perl/skeleton.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl -W -t -T
+
+# Perl Skeleton file.
+
+# useful commands:
+# validation check
+# perl -c -t -W template_boilerplate.pl
+# formatting
+# perltidy -b template_boilerplate.pl
+
+# part of perlstyle: http://perldoc.perl.org/perlstyle.html
+use strict;
+use warnings;
+
+use sigtrap;
+use diagnostics;
+
+# Code goes here
