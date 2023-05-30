5575795d59b5b1c3db223354a2d24ba5df295d24
alex.prodesk@example.com <alex.prodesk@example.com>
2023-05-30 18:24:51 -0400

    enh: Perl skeleton file.

--- bin/code_templates/perl/skeleton.pl
+++ bin/code_templates/perl/skeleton.pl
@@ -4,9 +4,9 @@
 
 # useful commands:
 # validation check
-# perl -c -t -W template_boilerplate.pl
-# formatting
-# perltidy -b template_boilerplate.pl
+# perl -c -t -W skeleton.pl
+# formatting (apt install perltidy)
+# perltidy -b skeleton.pl
 
 # part of perlstyle: http://perldoc.perl.org/perlstyle.html
 use strict;
@@ -15,4 +15,17 @@ use warnings;
 use sigtrap;
 use diagnostics;
 
+# apt install libmodern-perl-perl
+# use Modern::Perl '2017';
+
+# use Data::Dumper;
+
 # Code goes here
+
+my $usage = qq(
+Usage for $0:
+  $0 help    : display this help and exit
+  $0 version : display the version and exit
+);
+
+print("${usage}\n");
