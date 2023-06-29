7ef88903dc97cd81f6afaddb4d4f77c036a7cef8
alex.prodesk@example.com <alex.prodesk@example.com>
2023-06-29 18:27:30 -0400

    enh: shellcheck recommendations

--- bin/utils/files/clean_rename.sh
+++ bin/utils/files/clean_rename.sh
@@ -2,10 +2,11 @@
 
 # Rename each provided file with a more "acceptable" name
 
-if [ -z "$1" ]
+if [[ -z "$1" ]]
 then
   echo "provide a list of files"
   echo 'Replace all the characters that are not "[:alnum:].-/" with "_"'
+  echo '(the "+" sign is also replaced'
   echo 'If the name is OK do not attempt the mv command'
   exit 1
 fi
@@ -14,11 +15,12 @@ for current_name in "$@"
 do
   # Important to have . and / in the regex, without it the results
   # could be incorrect.
-  new_name=$(echo -n "$current_name" |  tr -c '[:alnum:].\-/' '_')
-  if [ "$new_name" == "$current_name" ]
+  new_name=$(echo -n "${current_name}" | tr -c '[:alnum:].\-/' '_')
+  new_name=$(echo -n "${new_name}" | tr '\+' '_')
+  if [[ "${new_name}" == "${current_name}" ]]
   then
-    echo "clean name already: $current_name"
+    echo "clean name already: ${current_name}"
   else
-    mv -v "$current_name" "$new_name"
+    mv -v "${current_name}" "${new_name}"
   fi
 done
