1c6ef6c494c0819b57505946d9198e3b06d53d76
alex.mele@example.com <alex.mele@example.com>
2025-01-12 19:35:11 -0500

    add: starting page option

--- bin/utils/network/web.sh
+++ bin/utils/network/web.sh
@@ -4,10 +4,11 @@ set -e
 
 touch ~/.w3m/history
 
-w3m -no-mouse -no-cookie -no-graph -s -W "$@"
+w3m -no-mouse -no-cookie -no-graph -s -W -v "$@"
 
 #     -no-mouse        don't use mouse
 #     -cookie          use cookie (-no-cookie: don't use cookie)
 #     -no-graph        use ASCII character for border of table and menu
 #     -s               squeeze multiple blank lines
 #     -W               toggle search wrap mode
+#     -v               start with the built-in page if no targets
