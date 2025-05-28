8ff26f439c172fde3ff2b03e2a2f06482cfdd146
alex.mele@example.com <alex.mele@example.com>
2025-05-27 21:47:15 -0400

    enh: a few more changes for w3m

--- bin/utils/network/web.sh
+++ bin/utils/network/web.sh
@@ -2,13 +2,24 @@
 
 set -e
 
+# in case the file does not exists
 touch ~/.w3m/history
 
-w3m -no-mouse -no-cookie -no-graph -s -W -v "$@"
+w3m \
+  -no-mouse \
+  -no-cookie \
+  -no-graph \
+  -s \
+  -W \
+  -v \
+  -o high-intensity=true \
+  -o tabstop=4 \
+  "$@"
 
-#     -no-mouse        don't use mouse
-#     -cookie          use cookie (-no-cookie: don't use cookie)
-#     -no-graph        use ASCII character for border of table and menu
-#     -s               squeeze multiple blank lines
-#     -W               toggle search wrap mode
-#     -v               start with the built-in page if no targets
+# -no-mouse    don't use mouse
+# -cookie      use cookie (-no-cookie: don't use cookie)
+# -no-graph    use ASCII character for border of table and menu
+# -s           squeeze multiple blank lines
+# -W           toggle search wrap mode
+# -v           start with the built-in page if no targets
+
